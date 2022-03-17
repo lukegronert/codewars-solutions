1968e2ffd3482962b9fe28d171a4484811645e17
Luke G <lukegronert@gmail.com>
2022-03-17 19:00:32 +0800

    Finished power of 2 8kyu

--- opposites-attract.js
+++
@@ -1,9 +0,0 @@
-function lovefunc(flower1, flower2){
-    if (flower1 % 2 === 0 && flower2 % 2 === 1) {
-        return true;
-    } else if (flower1 % 2 === 1 && flower2 % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
