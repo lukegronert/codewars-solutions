a333cddf16df83ef3f5cbb4b054da66cff12b165
lukegronert@gmail.com <lukegronert@gmail.com>
2022-05-17 21:16:43 -0700

    Solved Number of measurements to spot  the counterfeit coin 6kyu

---
+++ 6kyu/number-of-measurements-to-spot-the-counterfeit-coin.js
@@ -0,0 +1,27 @@
+// I found this interesting interview question just today:
+
+// 8 coins are given where all the coins have equal weight, except one. The odd one weights less than the others, not being of pure gold. In the worst case, how many iterations are actually needed to find the odd one out on a two plates scale.
+
+// I am asking you then to tell me what is the minimum amount of weighings it will take to measure n coins in every possible occurrence (including worst case scenario, ie: without relying on luck at all).
+
+// n is guaranteed to be a positive integer.
+
+// Tip: being able to think recursively might help here :p
+
+// Note: albeit this is more clearly a logical than a coding problem, do not underestimate (or under-rank) the kata for requiring not necessarily wizard-class coding skills: a good coder is a master of pattern recognition and subsequent optimization ;)
+
+function howManyMeasurements(n){
+    let count = 0;
+    while (n !== 1) {
+      if(n === 2 || n === 3) {
+        return count+1;
+      } else if (n === 8) {
+        return count + 2;
+      }
+      let remainder = n%3;
+      let third = (n-remainder)/3;
+      n=third + remainder;
+      count++;
+    }
+    return count;
+  }
